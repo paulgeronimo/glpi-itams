c5252967f05e9c0720b6d57e64d05828848a676e
Paul Geronimo <geronimo.paul@gmail.com>
2011-10-03 16:05:16 +0800

    test

--- index.php
+++ index.php
@@ -36,6 +36,7 @@
 // If config_db doesn't exist -> start installation
 define('GLPI_ROOT', '.');
 include (GLPI_ROOT . "/config/based_config.php");
+echo "test";
 
 if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
    include (GLPI_ROOT . "/inc/common.function.php");
